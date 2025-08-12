a694ec37591d6ea4c6abe678cece2a8b08a44231
Maksimens/Oriflame <Muskam.dan@gmail.com>
2025-08-12 12:15:19 +0300

    Soltion v2

--- network.tf
+++ network.tf
@@ -1,34 +1,20 @@
-resource "azurerm_virtual_network" "main" {
+data "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "internal" {
+data "azurerm_subnet" "internal" {
   name                 = "internal"
+  virtual_network_name = data.azurerm_network_interface.main.name
   resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = data.azurerm_resource_group.example.location
+data "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-vm858-30b0c52b"
   resource_group_name = data.azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
 }
 
-resource "azurerm_public_ip" "example" {
- name                = "${var.prefix}-public-ip"
- location            = data.azurerm_resource_group.example.location
- resource_group_name = data.azurerm_resource_group.example.name
- allocation_method   = "Static"
- sku = "Standard"
+data "azurerm_public_ip" "example" {
+ name                = "${var.prefix}-vm-ip-30b0c52b"
+  resource_group_name = data.azurerm_resource_group.example.name
 }
--- vm.tf
+++ vm.tf
@@ -1,48 +1,16 @@
-resource "azurerm_virtual_machine" "main" {
+data "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = data.azurerm_resource_group.example.location
   resource_group_name   = data.azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_B1s"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
-
-    provisioner "file" {
+}
+resource "null_resource" "provision_existing_vm" {
+  provisioner "file" {
     source = "./nginx_example.html"
     destination = "/home/testadmin/nginx_example.html"
     connection {
       type = "ssh"
       user = "testadmin"
       password = "Password1234!"
-      host = azurerm_public_ip.example.ip_address
+      host = data.azurerm_public_ip.example.ip_address
     }
   }
 
@@ -51,7 +19,7 @@ resource "azurerm_virtual_machine" "main" {
       type = "ssh"
       user = "testadmin"
       password = "Password1234!"
-      host = azurerm_public_ip.example.ip_address
+      host = data.azurerm_public_ip.example.ip_address
     }
     inline = [
       "sudo apt-get update -y",
